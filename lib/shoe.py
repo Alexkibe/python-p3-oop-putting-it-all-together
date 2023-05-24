75408a20a2671b4d0988727c3432590734880eb1
Alex Kibe <alex.kibe01@student.moringaschool.com>
2023-05-24 14:50:44 +0300

    Completed assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+
+class TestShoe:
+    def test_has_brand_and_size(self):
+        stan_smith = Shoe("Adidas", 9)
+        assert stan_smith.brand == "Adidas"
+        assert stan_smith.size == 9
